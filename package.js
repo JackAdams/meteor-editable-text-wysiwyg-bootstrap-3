1175fa0726051673ab4a14403f0affe26e5763ba
Brent Abrahams <brent_abrahams@yahoo.com>
2015-02-05 13:29:51 +0800

    Version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'babrahams:editable-text-wysiwyg-bootstrap-3',
   summary: 'Extends the babrahams:editable-text package to include a wysiwyg editor that uses bootstrap-3',
-  version: '0.3.10',
+  version: '0.3.11',
   git: 'https://github.com/jackadams/meteor-editable-text-wysiwyg-bootstrap-3.git'
 });
 
