f00b61212be2e002d3f9a11cd09a2a9371939146
Brent Abrahams <brent_abrahams@yahoo.com>
2016-04-15 18:54:07 +0800

    Updated dependencies

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'babrahams:editable-text-wysiwyg-bootstrap-3',
   summary: 'Extends the babrahams:editable-text package to include a wysiwyg editor that uses bootstrap-3',
-  version: '0.5.10',
+  version: '0.5.11',
   git: 'https://github.com/jackadams/meteor-editable-text-wysiwyg-bootstrap-3.git'
 });
 
@@ -9,15 +9,13 @@ Package.onUse(function(api) {
   
   api.versionsFrom('1.0');
   
-  api.use('babrahams:editable-text@0.8.10', ['client','server']);
+  api.use('babrahams:editable-text@0.8.11', ['client','server']);
   api.imply('babrahams:editable-text');
-  api.use('babrahams:editable-text-wysiwyg@0.5.10', 'client');
+  api.use('babrahams:editable-text-wysiwyg@0.5.11', 'client');
   api.use('templating', 'client');
   api.use('blaze', 'client');
   api.use('spacebars', 'client');
   api.use('jquery', 'client');
-  api.use(['twbs:bootstrap-noglyph@3.3.1','twbs:bootstrap@3.3.1', 'nemo64:bootstrap@3.3.1_1'], 'client', {weak: true});
-  api.use('fortawesome:fontawesome@4.4.0', 'client', {weak: true});
   
   api.add_files('lib/editable_text_wysiwyg_bootstrap_3.css', 'client');
   api.add_files('lib/editable_text_wysiwyg_bootstrap_3.html', 'client');
