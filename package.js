eda9664d52f58f6bf546b1fcd7b6152b0b88020c
Brent Abrahams <brent_abrahams@yahoo.com>
2016-08-11 20:00:12 +0800

    Added color picker

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'babrahams:editable-text-wysiwyg-bootstrap-3',
   summary: 'Extends the babrahams:editable-text package to include a wysiwyg editor that uses bootstrap-3',
-  version: '0.5.21',
+  version: '0.5.22',
   git: 'https://github.com/jackadams/meteor-editable-text-wysiwyg-bootstrap-3.git'
 });
 
@@ -9,9 +9,9 @@ Package.onUse(function(api) {
   
   api.versionsFrom('1.0');
   
-  api.use('babrahams:editable-text@0.8.21', ['client', 'server']);
+  api.use('babrahams:editable-text@0.8.22', ['client', 'server']);
   api.imply('babrahams:editable-text');
-  api.use('babrahams:editable-text-wysiwyg@0.5.21', 'client');
+  api.use('babrahams:editable-text-wysiwyg@0.5.22', 'client');
   api.use('templating', 'client');
   api.use('blaze', 'client');
   api.use('spacebars', 'client');
