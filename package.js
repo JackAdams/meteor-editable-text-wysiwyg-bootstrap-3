46c6e378cd4d78c6ed0aff1c2c10218597462e77
Brent Abrahams <brent_abrahams@yahoo.com>
2016-12-24 12:30:21 +1300

    Updated dependencies

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'babrahams:editable-text-wysiwyg-bootstrap-3',
   summary: 'Extends the babrahams:editable-text package to include a wysiwyg editor that uses bootstrap-3',
-  version: '0.5.33',
+  version: '0.5.34',
   git: 'https://github.com/jackadams/meteor-editable-text-wysiwyg-bootstrap-3.git'
 });
 
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
   
   api.use('babrahams:editable-text@0.8.31', ['client', 'server']);
   api.imply('babrahams:editable-text');
-  api.use('babrahams:editable-text-wysiwyg@0.5.32', 'client');
+  api.use('babrahams:editable-text-wysiwyg@0.5.33', 'client');
   api.use('templating', 'client');
   api.use('blaze', 'client');
   api.use('spacebars', 'client');
