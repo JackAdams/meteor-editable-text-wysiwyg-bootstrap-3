db6b7f921adea7b5944ffa5daee22383d2350625
Brent Abrahams <brent_abrahams@yahoo.com>
2021-07-22 00:13:30 +0800

    Updated for 2.3

--- package.js
+++ package.js
@@ -1,29 +1,26 @@
 Package.describe({
   name: 'babrahams:editable-text-wysiwyg-bootstrap-3',
   summary: 'Extends the babrahams:editable-text package to include a wysiwyg editor that uses bootstrap-3',
-  version: '0.6.15',
+  version: '0.6.21',
   git: 'https://github.com/jackadams/meteor-editable-text-wysiwyg-bootstrap-3.git'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   
-  api.versionsFrom('1.0');
+  api.versionsFrom(['1.8.2', '2.3']);
   
-  api.use('babrahams:editable-text@0.9.12', ['client', 'server']);
+  api.use('babrahams:editable-text@0.9.17', ['client', 'server']);
   api.imply('babrahams:editable-text');
-  api.use('babrahams:editable-text-wysiwyg@0.6.15', 'client');
-  api.use('templating', 'client');
-  api.use('blaze', 'client');
-  api.use('spacebars', 'client');
-  api.use('jquery', 'client');
+  api.use('babrahams:editable-text-wysiwyg@0.6.21', 'client');
+  api.use(['templating@1.3.2', 'blaze@2.3.4', 'spacebars@1.0.15', 'jquery@1.11.11'], 'client');
   
-  api.add_files('lib/editable_text_wysiwyg_bootstrap_3.css', 'client');
-  api.add_files('lib/editable_text_wysiwyg_bootstrap_3.html', 'client');
-  api.add_files('lib/editable_text_wysiwyg_bootstrap_3.js', 'client');
+  api.addFiles('lib/editable_text_wysiwyg_bootstrap_3.css', 'client');
+  api.addFiles('lib/editable_text_wysiwyg_bootstrap_3.html', 'client');
+  api.addFiles('lib/editable_text_wysiwyg_bootstrap_3.js', 'client');
   
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use('tinytest');
   api.use('babrahams:editable-text-wysiwyg');
   api.addFiles('editable-text-wysiwyg-tests.js');
